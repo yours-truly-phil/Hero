1da0f51f987dedcb77e6aa7ef7a91976689524f6
Philipp Seeger <philippbseeger@googlemail.com>
2020-10-30 16:55:13 +0100

    Initial commit

---
+++ android/src/main/java/io/horrorshow/android/AndroidLauncher.java
@@ -0,0 +1,17 @@
+package io.horrorshow.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import io.horrorshow.Hero;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+		initialize(new Hero(), configuration);
+	}
+}
---
+++ core/src/main/java/io/horrorshow/Hero.java
@@ -0,0 +1,7 @@
+package io.horrorshow;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Hero extends ApplicationAdapter {
+}
